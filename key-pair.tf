2954b67c55debdea4aa015b1f6e55a6f63abfeb6
Nicolas Yassuda <nicolas.yassuda@smartbrain.com>
2025-02-23 18:51:44 -0300

    ajusting keypair variable

--- key-pair.tf
+++ key-pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "CyberSecurityKeyPair" {
   key_name   = "CyberSecurityKeyPair"
-  public_key = file("/home/nicolasyassuda/.ssh/my-ec2-key.pub")
+  public_key = file("/home/home_server/.ssh/my-ec2-key.pub")
 }
